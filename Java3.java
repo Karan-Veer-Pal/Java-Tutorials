c9dd434bc3a247e6183d189061c3482192248e0c
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-26 12:17:13 +0530

    Add files via upload

---
+++ Java3.java
@@ -0,0 +1,17 @@
+// Day : 03 Variables and Data Types
+
+public class Java3 
+{
+    public static void main(String[] args) {
+        int num1 = 7; 
+        int num2 = 5; 
+        int num3 = 6; 
+        int sum = 0;
+        sum = num1 + num2 + num3;
+        System.out.print("The sum of three number is : ");
+        System.out.println(sum);
+    }
+}
+
+// println() print the line + add new line
+// print() print the line only
