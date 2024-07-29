60c161db3e3e464667ded8e51fa48474660adc00
Karan Veer Pal <karanpalveer@gmail.com>
2024-07-29 11:45:45 +0530

    Add files via upload

---
+++ Java4.java
@@ -0,0 +1,27 @@
+// Day : 04 Literals : A constant value which can be assigned to the variable is called as a Literal.
+// The value which can be stroed in any variables called Literal.
+
+public class Java4
+{
+    public static void main(String[] args) {
+        byte age = 34; // byte literals
+        System.out.println(age);
+
+        char ch = 'A'; // Character literals
+        System.out.println(ch);
+
+        float f1 = 4.5f; // Float literals, Otherwise, Default value is f or F
+        System.out.println(f1);
+
+        double d1  = 34D; // Double literals, Otherwise, Default value is d or D
+        System.out.println(d1);
+
+        String str = "Karan"; // String literals
+        System.out.println(str);
+
+        Boolean a = true; // Boolean literals
+        System.out.println(a);
+    }
+}
+
+// Keywords : Words which are reserved and used by the Java compiler. They cannot be used as an Identifier.
