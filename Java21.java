997041eb1fa6d6421571ecc010d9693ed7202eae
Karan Veer Pal <karanpalveer@gmail.com>
2024-08-23 15:59:51 +0530

    Add files via upload

---
+++ Java21.java
@@ -0,0 +1,23 @@
+// Day : 21 While Loop
+
+public class Java21
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i < 3)
+        {
+            System.out.println("Demonstration of while loop = " + i);
+            i++;
+        }
+        System.out.println("Finished Execute while loop");
+
+        // Quick Quiz : WAP to print natural numbers from 100 to 200?
+        int natural_number = 100;
+        while(natural_number <= 200)
+        {
+            System.out.println("Natural Number : " + natural_number);
+            natural_number++;
+        }
+    }
+}
