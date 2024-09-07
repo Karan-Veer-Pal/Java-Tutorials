ff40c521bb7dbf4875fef03cbd51f13ffe26c256
Karan Veer Pal <karanpalveer@gmail.com>
2024-09-07 14:33:53 +0530

    Add files via upload

---
+++ Java36.java
@@ -0,0 +1,8 @@
+// Day : 36 Introduction to OOPs
+
+// ➡️ Introduction to Object Oriented Programming.
+// ➡️ What is Object Oriented Programming?
+// ➡️ What is DRY?
+// ➡️ Overview of Class.
+// ➡️ Overview of Object.
+// ➡️ How to model a problem in OOPs.
