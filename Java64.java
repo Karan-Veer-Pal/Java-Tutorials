87b9c18fb35145db4e277a178b49561404a5baa6
Karan Veer Pal <karanpalveer@gmail.com>
2024-12-17 19:06:28 +0530

    Add files via upload

---
+++ Java64.java
@@ -0,0 +1,21 @@
+//package karan1;
+
+public class Java64
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I am Class Java's main method!");
+    }
+}
+
+// Summary Of The Day :
+// -> We can make a class file by run javac Java64 command in terminal window. After that Java64.class file build.
+// -> Now, when we delete the Java64.java file, use the code of the Java64.java file by using Java64.class.
+// -> How to run the code : First command is javac Java64.java & Second command is java Java64 this command is run the Java64.class file.
+// -> Java64.class file work Without Java64.java file exist in a folder. If we delete the class file then it can't work.
+// -> Run the code of Java64.java Code by giving this command : java Java64.java
+
+// -> We use two type of packages :
+// -> One is build in package which is come defaultly in IntelliJ Idea.
+// -> Another one is made by the user. Packages means 2 or more java file of similar or one type of doing a work then these file is converted into a package.
+// -> When we make package, we write the top of the file is : package package_name; in every code file which are come in a packages.
