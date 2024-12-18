bda90082bef9da01ef6e2070a7d696f468cc37c1
Karan Veer Pal <karanpalveer@gmail.com>
2024-12-18 16:35:36 +0530

    Add files via upload

---
+++ Java65.java
@@ -0,0 +1,13 @@
+// Day : 65 Creating a Java Package
+
+//import java.util.Scanner;
+import java.util.*; // --> import all the classes.
+
+public class Java65
+{
+    public static void main(String[] args)
+    {
+        Scanner variable = new Scanner(System.in);
+        // java.util.Scanner variable = new java.util.Scanner(System.in); It is also work, but we don't need to import the Scanner class on a top of a code.
+    }
+}
