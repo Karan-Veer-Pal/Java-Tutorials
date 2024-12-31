09075e2afa034ce46ccc44732fd21b45a115517c
Karan Veer Pal <karanpalveer@gmail.com>
2024-12-31 19:02:40 +0530

    Add files via upload

---
+++ Java78.java
@@ -0,0 +1,9 @@
+// Day : 78 Error and Exception
+
+public class Java78
+{
+    public static void main(String[] args)
+    {
+        // int a = 5 --> It is called Syntax Error. Semicolon ; is missing.
+    }
+}
