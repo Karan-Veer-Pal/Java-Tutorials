bf0869297009e51d97e55c5bdcfbddf4e8827618
Karan Veer Pal <karanpalveer@gmail.com>
2025-01-12 18:29:31 +0530

    Add files via upload

---
+++ Java89.java
@@ -0,0 +1,17 @@
+// Day : 89 Collection Hierarchy : Diagram + Theory
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Java89
+{
+    public static void main(String[] args)
+    {
+        // ArrayList a;
+        // Set s;
+        // TreeSet ts;
+    }
+}
+
+
