4c13b88e00c3f6451618746a83937953b95c5648
Karan Veer Pal <karanpalveer@gmail.com>
2025-01-13 18:51:02 +0530

    Add files via upload

---
+++ Java90.java
@@ -0,0 +1,9 @@
+// Day : 90 Best Way To Use Java Docs : Take General Knowledge : Use Java Oracle Original Documentation.
+
+public class Java90
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
