02e7dfcc001a331a8f53408c0e0d9b5fc2270ea6
Karan Veer Pal <karanpalveer@gmail.com>
2025-01-20 20:26:59 +0530

    Add files via upload

---
+++ Java96.java
@@ -0,0 +1,10 @@
+// Day : 96 Time
+
+public class Java96
+{
+    public static void main(String[] args)
+    {
+        System.out.println(System.currentTimeMillis() + " MilliSeconds are done through 1 Jan 1970." );
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365 + " Years are done through 1 Jan 1970." );
+    }
+}
